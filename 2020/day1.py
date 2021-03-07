4a3e5b9eec8b1dd9e22e47323efe45bf1b6f9fc5
Samuel Tang <samywtang@gmail.com>
2021-03-06 22:22:52 -0500

    Add day 1 part two, completed day 1

--- 2020/day1.py
+++ 2020/day1.py
@@ -411,9 +411,27 @@ def expense_report(list):
             k -= 1
         elif list[i] + list[k] < 2020:
             i += 1
-
         else:
             return list[i] * list[k]
 
 
 print(expense_report(list))
+
+
+def part_2(list):
+    list.sort()
+    curr = 0
+    while curr < len(list) - 2:
+        left = curr + 1
+        right = len(list) - 1
+        while left < right:
+            if list[curr] + list[left] + list[right] > 2020:
+                right -= 1
+            elif list[curr] + list[left] + list[right] < 2020:
+                left += 1
+            else:
+                return list[curr] * list[left] * list[right]
+        curr += 1
+
+
+print(part_2(list))
