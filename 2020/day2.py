4bb3b634e35e81a59e2bda3194536224932ce271
Samuel Tang <samywtang@gmail.com>
2021-03-07 21:06:03 -0500

    Add part two of AoC day 2

--- 2020/day2.py
+++ 2020/day2.py
@@ -12,6 +12,18 @@ def part_one(freq, letter, arr):
     return pw_count
 
 
+def part_two(freq, letter, arr):
+    pw_count = 0
+    for i in range(len(arr)):
+        pos_one = int(freq[i][0]) - 1
+        pos_two = int(freq[i][1]) - 1
+        if letter[i] == arr[i][pos_one] and letter[i] == arr[i][pos_two]:
+            continue
+        elif letter[i] == arr[i][pos_one] or letter[i] == arr[i][pos_two]:
+            pw_count += 1
+    return pw_count
+
+
 with open("day2.txt") as _file:
     line = [line.split() for line in _file]
     freq = []
@@ -22,3 +34,4 @@ with open("day2.txt") as _file:
         letter.append(i[1].replace(":", ""))
         arr.append(i[2])
     print(part_one(freq, letter, arr))
+    print(part_two(freq, letter, arr))
