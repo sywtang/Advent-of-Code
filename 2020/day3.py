313441c7b53ee35e3c2051d3e8e65e05665ec29b
Samuel Tang <samywtang@gmail.com>
2021-03-10 22:23:44 -0500

    Add revised solution to day3 part two

--- 2020/day3.py
+++ 2020/day3.py
@@ -2,26 +2,26 @@ with open("day3.txt", "r") as fd:
     lines = [line.rstrip() for line in fd]
 
 
-def part_one(slope, jump=1):
+def part_one(direction, jump=0):
     trees = 0
-    right = slope
-    for i in range(jump, len(lines)):
+    right = direction
+    for i in range(jump, len(lines), jump):
         if lines[i][right % len(lines[i])] == "#":
             trees += 1
-        right += slope
+        right += direction
     return trees
 
 
 # take in list of arguments where n elements represents slopes
 def part_two():
-    slope_one = part_one(1)
-    slope_two = part_one(3)
-    slope_three = part_one(5)
-    slope_four = part_one(7)
+    slope_one = part_one(1, 1)
+    slope_two = part_one(3, 1)
+    slope_three = part_one(5, 1)
+    slope_four = part_one(7, 1)
     slope_five = part_one(1, 2)
 
     return slope_one * slope_two * slope_three * slope_four * slope_five
 
 
-print(part_one(3))
+print(part_one(3, 1))
 print(part_two())
