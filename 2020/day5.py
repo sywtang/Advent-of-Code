4c7ae62e8259f6c431aa1e16922414b16925a893
Samuel Tang <samywtang@gmail.com>
2021-05-30 00:55:16 -0400

    Adds full solution for part one day 5, refactoring required

--- 2020/day5.py
+++ 2020/day5.py
@@ -15,5 +15,54 @@ SEAT_2 = "FB"
 # ex: (63 - 0) // 2 = 31 -> 63 - 31 = 32 => 63, 32
 
 
+# Return highest seat ID
 def part_one(data):
-    return data
+    highest_seat = 0
+
+    for i in range(len(data)):
+        seat = partition_seats(data[i])
+        if seat > highest_seat:
+            highest_seat = seat
+    return highest_seat
+
+
+def partition_seats(data):
+    row_lower = 0
+    row_upper = 127
+
+    col_lower = 0
+    col_upper = 7
+
+    row = 0
+    col = 0
+    for i in range(len(data)):
+        if data[i] == "F":
+            new_half = (row_upper - row_lower) // 2
+            row_upper = row_lower + new_half
+        elif data[i] == "B":
+            new_half = (row_upper - row_lower) // 2
+            row_lower = row_upper - new_half
+        if i == 6:
+            row = (
+                min(row_lower, row_upper)
+                if data[i] == "F"
+                else max(row_lower, row_upper)
+            )
+        if data[i] == "R":
+            new_half = (col_upper - col_lower) // 2
+            col_lower = col_upper - new_half
+        elif data[i] == "L":
+            new_half = (col_upper - col_lower) // 2
+            col_upper = col_lower + new_half
+        if i == 9:
+            col = (
+                min(col_lower, col_upper)
+                if data[i] == "L"
+                else max(col_lower, col_upper)
+            )
+
+    seat = row * 8 + col
+    return seat
+
+
+print(part_one(lines))
